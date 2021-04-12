50728a809d63c8be860aed3f09ec3cd20f5ab68b
Prabhdeep Kainth <pdkainth@gmail.com>
2021-04-11 18:24:19 -0700

    got more cuorse info

--- schedule-tool/excel_db/course_web_info.py
+++ schedule-tool/excel_db/course_web_info.py
@@ -13,6 +13,10 @@ class ClassWebInfo:
             sys.exit()
         
         self.__genGE_List()
+        self.__genUnits()
+        self.__genDesc()
+        self.__genSeats()
+        self.__genMax_Enroll()        
 
     def __genGE_List(self):
         self.ge_list = []
@@ -29,9 +33,64 @@ class ClassWebInfo:
             ge = "".join(ge)
             self.ge_list.append(ge)
     
+    def __genUnits(self):
+        self.units = ""
+        text = html2text.html2text(self.r.text)
+        match = re.search(r"\*\*Units:\*\*\s(\S)", text, re.S)
+        if not match:
+            return
+
+        self.units = match.group(1)                        
+
+    def __genDesc(self):
+        self.desc = ""
+        text = html2text.html2text(self.r.text)
+        match = re.search(r"\*\*Description:\*\*\s*(\S.*?)\s*---", text, re.S)
+        
+        if not match:
+            return
+
+        self.desc = match.group(1)                        
+    
+    def __genSeats(self):
+        self.seats = ""
+        text = html2text.html2text(self.r.text)
+        match = re.search(r"\*\*Available Seats:\*\*\s(\S)", text, re.S)
+        if not match:
+            return
+
+        self.seats = match.group(1)                        
+
+    def __genMax_Enroll(self):
+        self.max_enroll = ""
+        text = html2text.html2text(self.r.text)
+        match = re.search(r"\*\*Maximum Enrollment:\*\*\s(\S*)", text, re.S)
+        if not match:
+            return
+
+        self.max_enroll = match.group(1)                        
+
     def getGE_List(self):
         return self.ge_list
 
+    def getUnits(self):
+        return self.units
+
+    def getDesc(self):
+        return self.desc
+    
+    def getSeats(self):
+        return self.seats
+    
+    def getMax_Enroll(self):
+        return self.max_enroll
+
 if __name__ == "__main__":
-    test = ClassWebInfo(62905, 202103)
+    test = ClassWebInfo(44644, 202101)
     print(test.getGE_List())
+    print(test.getUnits())
+    print(test.getDesc())
+    print(test.getSeats())
+    print(test.getMax_Enroll())
+
+
