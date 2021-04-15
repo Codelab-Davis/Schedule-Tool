b46739efa5209d9dae4f5c8b067e7994b94c3d81
Minh-Tu Nguyen <minngu@ucdavis.edu>
2021-04-14 18:51:45 -0700

    Co-authored-by: libbyhopkins <libbyhopkins@users.noreply.github.com> Co-authored-by: salmakhassen <salmakhassen@users.noreply.github.com>

--- schedule-tool/frontend/src/index.js
+++ schedule-tool/frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './fonts/ProximaNova-Regular.otf'
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
