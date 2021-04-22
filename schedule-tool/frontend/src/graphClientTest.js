1114e705d4281ad09d93fb199544d278d79ff92c
James Junaidi <james.junaidi@gmail.com>
2021-04-21 21:24:34 -0700

    broken graph client test

--- schedule-tool/frontend/src/graphClientTest.js
+++ schedule-tool/frontend/src/graphClientTest.js
@@ -3,18 +3,42 @@ import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
 
 
+
+// var name = "ECS120"
+
+// const getCoursesQuery = gql`
+//     query ($names: String!) {  
+//         course_id(id: $names) {
+//             name
+//             instructor
+//         }
+//     }
+// `;
+
+const params = { name: "ECS120"}
+
+// `www.google.com/${name}`
 const getCoursesQuery = gql`
-{
-  course_id(id: "AAS178") {
-    name
-    instructor
-  }
-}
+    {         
+        course_id(id: "ECS120") {
+            name
+            instructor
+        }
+    }
 `;
+
+// const getCoursesQuery = gql`
+// {
+//   courses {
+//     name
+//     instructor
+//   }
+// }
+// `;
 
 class GraphClientTest extends Component {
     render() {
-        console.log(this.props.data.courses);
+        console.log(this.props.data);
         console.log("hello there");
         return (
             <div>
