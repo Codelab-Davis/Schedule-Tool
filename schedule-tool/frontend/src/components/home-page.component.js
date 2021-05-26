fcf2aedf85cc858c1de2e853ae006b1078dfe6e6
James Junaidi <james.junaidi@gmail.com>
2021-05-26 16:46:13 -0700

    i

--- schedule-tool/frontend/src/components/home-page.component.js
+++ schedule-tool/frontend/src/components/home-page.component.js
@@ -38,7 +38,7 @@ export default class HomePage extends Component {
                     <div class="container">
                         <div class="row">
                             <div id="featuresleft" class="col-md-5 d-flex justify-content-center">
-                                <a href="grades"> 
+                                <a href="/grades"> 
                                     <div id="gradecard" class="card">
                                         <div class="card-body">
                                             <div id="gradecardimage"><img src={gradeimage} id="gradeimage"/></div>
@@ -50,7 +50,7 @@ export default class HomePage extends Component {
                             </div>
                             <div class="col-md-1 justify-content-center"></div>
                             <div id="featuresright" class="col-md-5 justify-content-center">
-                                <a href="search">
+                                <a href="/enrollment">
                                     <div id="enrollmentcard" class="card">
                                         <div id="enrollmentcardimage"><img src={enrollmentimage} id="enrollmentimage"/></div>
                                         <div id="enrollmentcardtitle"><b>Enrollment Probability</b></div>
